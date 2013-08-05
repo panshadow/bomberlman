ad1272e7a808fc80bf56435ceb372599df7dbde4
Oleg Neizhkasha <panshdw@gmail.com>
2013-08-05 16:41:45 +0300

    added simple WebSocket client

--- priv/js/srcipt.js
+++ priv/js/srcipt.js
@@ -1,3 +1,21 @@
-(function(){
-  
-}(this));
+$(function(){
+  var header = $('h1#header');
+  var ws = new WebSocket('ws://' + window.location.host + '/ws/');
+  header.addClass('ping');
+
+  ws.onmessage = function(evt){
+    console.log(evt.data);
+    header.toggleClass('pong');
+  }
+
+  var wsInterval = setInterval(function(){
+    ws.send('ping');
+  },1000);
+
+  window.wsPingPongStop = function(){
+    clearInterval(wsInterval);
+  }
+  header.on('click',function(){
+    wsPingPongStop();
+  });
+});
