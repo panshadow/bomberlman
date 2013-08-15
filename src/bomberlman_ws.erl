8f0024070b8ce0eebc1b960d6d558ed78e3de00b
Oleg Neizhkasha <panshdw@gmail.com>
2013-08-15 17:57:31 +0300

    Sort of clean up

--- src/bomberlman_ws.erl
+++ src/bomberlman_ws.erl
@@ -1,7 +1,7 @@
 -module(bomberlman_ws).
 -behavior(cowboy_websocket_handler).
 
--record(wss, {count=0}).
+-record(wss, {ticks=0}).
 
 -export([init/3,
     websocket_init/3,
@@ -13,28 +13,33 @@ init({_TransportName, _Protocol}, _Req, _Opts) ->
   {upgrade, protocol, cowboy_websocket}.
 
 
-wsmsg(Type,Data) ->
-  {[{type, Type},{data, Data}]}.
+
+wsmsg(new, Type) -> wsmsg(new, Type, []);
+wsmsg(type, {[{<<"type">>, Type},{<<"data">>, _}]}) -> Type;
+wsmsg(data, {[{<<"type">>, _},{<<"data">>, Data}]}) -> Data.
+
+wsmsg(new, Type, Data) ->
+  {[{type, Type},{data, {Data}} ]}.
+
 
 websocket_init(_TransportName, Req, _Opts) ->
   {{RemoteHost,RemotePort},_} = cowboy_req:peer(Req),
-  Msg = wsmsg(<<"NOTIFY">>,{[
-      {<<"host">>, tuple_to_list(RemoteHost)}, 
-      {<<"port">>, RemotePort}
-    ]}
-  ),
+  Msg = wsmsg(new, <<"NOTIFY">>,[
+      {host, tuple_to_list(RemoteHost)}, 
+      {port, RemotePort}
+  ]),
   gproc:reg({p,l,bomberlman_arena}),
-  gproc:send({p,l,bomberlman_arena},{notify, jiffy:encode(Msg)}),
-  {ok, Req, #wss{count=0}, hibernate}.
+  gproc:send({p,l,bomberlman_arena},{notify, Msg}),
+  {ok, Req, #wss{ticks=0}, hibernate}.
 
 
 websocket_handle({text, JSON}, Req, State) ->
-  {[{<<"type">>,Type},{<<"data">>,_Data}]} = jiffy:decode(JSON),
-  Resp = case Type of
-    <<"PING">> -> wsmsg(<<"PONG">>,{[]});
-    _ -> wsmsg(<<"UNKNOWN">>,{[]})
-  end,
-  {reply, {text, jiffy:encode(Resp)}, Req, State#wss{count=State#wss.count+1}, hibernate };
+  Msg = jiffy:decode(JSON),
+  Type = wsmsg(type, Msg),
+  case  Type of
+    <<"PING">> -> ping_pong(Req, State);
+    _ -> reply(wsmsg(new, <<"UNKNOWN">>),Req, State)
+  end;
 
 
 websocket_handle(_Any, Req, State) ->
@@ -42,8 +47,7 @@ websocket_handle(_Any, Req, State) ->
   {ok, Req, State}.
 
 websocket_info({notify, Msg}, Req, State) ->
-  io:format("notify> ~p",[Msg]),
-  {reply, {text, Msg}, Req, State#wss{count=State#wss.count+1}, hibernate };
+  reply(Msg, Req, State);
 
 websocket_info(_Info, Req, State) ->
   io:format("info>\n"),
@@ -52,3 +56,14 @@ websocket_info(_Info, Req, State) ->
 websocket_terminate(_Reason, _Req, _State) ->
   ok.
 
+
+% ----
+next_tick(State) -> State#wss{ticks=State#wss.ticks+1}.
+
+reply(Msg, Req, State) ->
+  io:format("R>[~p]~n",[Msg]),
+  {reply, {text, jiffy:encode(Msg)}, Req, next_tick(State), hibernate}.
+
+ping_pong(Req, State) ->
+  Msg = wsmsg(new, <<"PONG">>,[{ticks, State#wss.ticks}]),
+  reply(Msg, Req, State).
